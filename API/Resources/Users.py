33d7c5e9f6c3039b5a384df6f74522031de32488
tivo_k <tivo.kenevil@gmail.com>
2021-09-25 17:20:38 -0500

    Started User Resource

---
+++ API/Resources/Users.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource, reqparse
+
+_user_data_parser = reqparse.RequestParser()
+
+_user_data_parser.add_argument(
+                          
+                    )
+
+class UserLogin(Resource):
+    pass
